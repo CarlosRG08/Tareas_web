1d40d97de3bf912f793fd5f44187d979342ba4d1
CarlosRG08 <RodriGomezCM2004@gmail.com>
2023-06-07 14:50:56 -0600

    TAREA_1_DESARROLLO_WWEB

---
+++ src/main/java/com/tarea_web_1/TareaWeb1Application.java
@@ -0,0 +1,13 @@
+package com.tarea_web_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea_web_1/TareaWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea_web_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
